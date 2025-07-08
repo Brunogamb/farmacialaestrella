3699f1d235ec654cf6dc7e2622e996cfd4e14ab7
Brunogamb <brunogamberini24@gmail.com>
2025-07-08 11:33:17 -0300

    actualizacionofertas

--- ofertas.js
+++ ofertas.js
@@ -53,42 +53,6 @@ const ofertas = [
         descripcion: "30% de descuento - SiempreBien",
         imagen: "IMGOFERTAS/oferta (9).jpg"
     },
-    {
-        precio: "$3.874 C/U",
-        nombre: "Dove Antitranspirante Original En Barra X50gr",
-        descripcion: "30% de descuento - SiempreBien",
-        imagen: "IMGOFERTAS/oferta (10).jpg"
-    },
-    {
-        precio: "$2.759 C/U",
-        nombre: "Colgate Cepillo Sensitive Pro Alivio Suave x1u",
-        descripcion: "35% de descuento - SiempreBien",
-        imagen: "IMGOFERTAS/oferta (11).jpg"
-    },
-    {
-        precio: "$3.564 C/U",
-        nombre: "Alta Moda Mascara Capilar Super Olis x300ml",
-        descripcion: "45% de descuento - SiempreBien",
-        imagen: "IMGOFERTAS/oferta (12).jpg"
-    },
-    {
-        precio: "$1.114 C/U / 1.399 C/U",
-        nombre: "Lady Soft Toallas Femenina Normal/Ultradelgada Tela Suave Con Alas x8u",
-        descripcion: "45% de descuento - SiempreBien",
-        imagen: "IMGOFERTAS/oferta (13).jpg"
-    },
-    {
-        precio: "$4.289 C/U",
-        nombre: "Veet Banda Depilatoria Piel Sensible x16u",
-        descripcion: "45% de descuento - SiempreBien",
-        imagen: "IMGOFERTAS/oferta (14).jpg"
-    },
-    {
-        precio: "$4.459 C/U",
-        nombre: "Oral B Crema Dental Detox Sensible x75ml",
-        descripcion: "50% de descuento - SiempreBien",
-        imagen: "IMGOFERTAS/oferta (15).jpg"
-    },
 ];
 
 window.addEventListener("DOMContentLoaded", () => {
