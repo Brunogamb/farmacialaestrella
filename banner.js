8a71cc5887449b2dc1278e6fd9155f68db2395e3
Brunogamb <brunogamberini24@gmail.com>
2025-07-08 18:27:56 -0300

    cambios en footer bottom

--- banner.js
+++ banner.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const banners = [
         {
-            imagen: "IMGOFERTAS/oferta banner.jpg?v=13",
+            imagen: "IMGOFERTAS/oferta banner.jpg?v=14",
             enlace: "ofertas.html",
             alt: "Oferta destacada"
         },
