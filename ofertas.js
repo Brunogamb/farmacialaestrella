3eb912b0279a80c1339feb556161ce88f5f69811
Brunogamb <brunogamberini24@gmail.com>
2025-07-11 09:50:18 -0300

    actualización de precios ofertas SiempreBien

--- ofertas.js
+++ ofertas.js
@@ -1,56 +1,56 @@
 const ofertas = [
     {
-        precio: "$5.184 C/U",
-        nombre: "Capilatis Shampoo y Acondicionador Ortiga Concentrado x420ml",
-        descripcion: "45% de descuento - SiempreBien",
+        precio: "$3.419 C/U",
+        nombre: "",
+        descripcion: "35% de descuento - SiempreBien",
         imagen: "IMGOFERTAS/oferta (1).jpg"
     },
     {
-        precio: "$4.779 C/U",
-        nombre: "Old Spice Desodorante Leña x50g",
+        precio: "$6.809 C/U",
+        nombre: "",
         descripcion: "35% de descuento - SiempreBien",
         imagen: "IMGOFERTAS/oferta (2).jpg"
     },
     {
-        precio: "$3.694 C/U",
-        nombre: "Jonson's Shampoo y Acondicionador Ortiga Concentrado x420ml",
-        descripcion: "30% de descuento - SiempreBien",
+        precio: "$20.814 C/U",
+        nombre: "",
+        descripcion: "35% de descuento - SiempreBien",
         imagen: "IMGOFERTAS/oferta (3).jpg"
     },
     {
-        precio: "$1.104 C/U",
-        nombre: "Espadol Dettol Jabón Active Deo 80g",
-        descripcion: "45% de descuento - SiempreBien",
+        precio: "$2.839 C/U",
+        nombre: "",
+        descripcion: "20% de descuento - SiempreBien",
         imagen: "IMGOFERTAS/oferta (4).jpg"
     },
     {
-        precio: "$13.839 C/U",
-        nombre: "Aveno Shampoo y Acondicionador Restaurador Capilar x250ml",
-        descripcion: "35% de descuento - SiempreBien",
+        precio: "$14.304 C/U",
+        nombre: "",
+        descripcion: "30% de descuento - SiempreBien",
         imagen: "IMGOFERTAS/oferta (5).jpg"
     },
     {
-        precio: "$5.409",
-        nombre: "Gillette Cartuchos Para Afeitar Mach3 x2",
-        descripcion: "20% de descuento - SiempreBien",
+        precio: "$6.599 C/U",
+        nombre: "",
+        descripcion: "45% de descuento - SiempreBien",
         imagen: "IMGOFERTAS/oferta (6).jpg"
     },
     {
-        precio: "$5.829 C/U",
-        nombre: "Gillette Desodorante Cool Wave x93gr",
-        descripcion: "35% de descuento - SiempreBien",
+        precio: "$31.069 C/U",
+        nombre: "",
+        descripcion: "25% de descuento - SiempreBien",
         imagen: "IMGOFERTAS/oferta (7).jpg"
     },
     {
-        precio: "$2.694 C/U",
-        nombre: "Rexona Antitranspirante Hombre/ V8/Invis./ Xtracool En Aerosol x150ml",
-        descripcion: "30% de descuento - SiempreBien",
+        precio: "$14.254 C/U",
+        nombre: "",
+        descripcion: "20% de descuento - SiempreBien",
         imagen: "IMGOFERTAS/oferta (8).jpg"
     },
     {
-        precio: "$3.694 C/U",
-        nombre: "Rexona Antitransp. Mujer Cotton Dry/Bamb/Antibac/Active M./Nutritive En Aerosol x150ml",
-        descripcion: "30% de descuento - SiempreBien",
+        precio: "$4.574 C/U",
+        nombre: "",
+        descripcion: "50% de descuento - SiempreBien",
         imagen: "IMGOFERTAS/oferta (9).jpg"
     },
 ];
