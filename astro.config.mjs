6719eeb4741a52cf9fe61c973bf772507694b4cb
Brunogamb <brunogamberini24@gmail.com>
2025-10-14 22:55:10 -0300

    Add sitemap integration and configuration; create tasks for building and verifying sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://www.farmacialaestrella.com.ar',
+  integrations: [sitemap()],
   devToolbar: {
     enabled: false
   }
